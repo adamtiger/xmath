19310b1e6da52571a8731c5b63998f8443cebcb7
Adam Budai <budai8adam@gmail.com>
2024-11-28 00:06:35 +0100

    log skeleton

---
+++ srcs/log.cpp
@@ -0,0 +1,12 @@
+#include "log.hpp"
+
+f64 logarithm(const f64 x)
+{
+    return 0.0;
+}
+
+f32 logarithm(const f32 x)
+{
+    
+    return 0.f;
+}
---
+++ srcs/log.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "core.hpp"
+
+XMATH_API f64 logarithm(const f64 x);
+XMATH_API f32 logarithm(const f32 x);
+
