1ec8e9066a3c8ffd163545fa295ab9d6a6bf2f70
Adam Budai <budai8adam@gmail.com>
2024-11-27 23:53:37 +0100

    exp can support full range and negative values too

--- srcs/exp.cpp
+++ srcs/exp.cpp
@@ -7,10 +7,13 @@ f64 exponent(const f64 x)
 
 f32 exponent(const f32 x)
 {
-    f32 z = x * C_LOG2_E;
+    f32 z = x * C_LOG2_E * 0.25f;
 
-    i32 zi0 = static_cast<i32>(z);
+    i32 is_z_neg = static_cast<i32>(z < 0);
+
+    i32 zi0 = (static_cast<i32>(z) - is_z_neg);
     f32 z1 = (z - static_cast<f32>(zi0)) * 0.5f;
+    zi0 = zi0 * (1 - 2 * is_z_neg);
 
     f32 y0 = static_cast<f32>(2 << (zi0 - 1));
 
@@ -23,7 +26,11 @@ f32 exponent(const f32 x)
     y1 = y1 * xln2 + 1.f;
 
     // integer and remainder, to form the final res.
+    y0 = y0 * (1.f - (f32)is_z_neg) + 1.f / y0 * (f32)is_z_neg;
+
     f32 y = y0 * y1 * y1;
+    y = y * y;
+    y = y * y;
 
     return y;
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     std::cout << "XMath implementations with intrinsics." << std::endl;
 
-    f32 x = 6.8f;
+    f32 x = 59.87f;
     f32 y = exponent(x);
 
     std::cout << "Expected: " << expf(x) << " - Actual: " << y << std::endl;
